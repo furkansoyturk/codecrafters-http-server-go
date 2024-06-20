36b165d7a2f4da1562a92e41f15900f5f35cad0f
furkansoyturk <furkansoyturk@icloud.com>
2024-06-20 13:30:25 +0300

    pass stage

--- app/server.go
+++ app/server.go
@@ -63,7 +63,22 @@ func requestHandler(conn net.Conn) {
 	requestFields := strings.Fields(request)
 	httpRequest.Method = requestFields[0]
 	urlParts := strings.Split(requestFields[1], "/")
-	httpRequest.Body = requestFields[len(requestFields)-1]
+
+	var count int
+	var body string
+	for i, s := range requestFields {
+		if strings.ToLower(s) == "content-length:" {
+			count = 2 + i
+		}
+	}
+
+	for i := count; i < len(requestFields); i++ {
+		body += requestFields[i]
+		if i < len(requestFields)-1 {
+			body += " "
+		}
+	}
+	httpRequest.Body = body
 
 	switch len(urlParts) {
 	case 2:
@@ -98,8 +113,6 @@ func responseHander(req httpRequest, conn net.Conn) {
 		length := len(req.PathParam)
 		response = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %v\r\n\r\n%v", length, req.PathParam)
 	case "files":
-
-		log.Println("requested file -> " + req.PathParam)
 		if req.Method == "GET" {
 			length, data, err := readFile(req.PathParam)
 			if err != nil {
@@ -107,13 +120,11 @@ func responseHander(req httpRequest, conn net.Conn) {
 			} else {
 				response = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: %v\r\n\r\n%v", length, data)
 			}
-
 		}
 
 		data := []byte(req.Body)
 		if req.Method == "POST" {
 			dir := "/tmp/data/codecrafters.io/http-server-tester/" + req.PathParam
-			log.Println("POST REQ. " + req.Method)
 			os.Create(dir)
 			os.WriteFile(dir, data, 666)
 			response = fmt.Sprintf("HTTP/1.1 201 Created\r\nContent-Type: application/octet-stream\r\nContent-Length: %v\r\n\r\n%v", len(req.Body), req.Body)
